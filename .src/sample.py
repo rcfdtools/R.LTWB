c88068bbd13f1fd46ecd858944e504be66f94a8f
r.cfdtools <r.cfdtools@gmail.com>
2022-06-29 15:39:46 -0500

    General update.

--- .src/sample.py
+++ .src/sample.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
 # Date difference sample
-fecha_instalacion = '1973-11-02'
-fecha_suspension = '2022-06-28'
+fecha_instalacion = '1972-09-14'
+fecha_suspension = '2019-07-02'
 diff_date = datetime.strptime(fecha_suspension, '%Y-%m-%d') - datetime.strptime(fecha_instalacion, '%Y-%m-%d')
 
 print(diff_date.days/365)
