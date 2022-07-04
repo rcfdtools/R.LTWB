bcf848cbe153ab099ad03d377cdbf82190d26573
r.cfdtools <r.cfdtools@gmail.com>
2022-07-04 11:19:36 -0500

    General update.

--- .src/sample.py
+++ .src/sample.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 # Basic date difference sample
-import dateutil.utils
+#import dateutil.utils
 
 installation_date = '1972-09-14'
 suspension_date = '2019-07-02'
@@ -16,7 +16,7 @@ print('Basic date difference sample\n'
 installation_date = '1972-09-14'
 suspension_date = '' # Use '' for not suspension date
 tw_end_date = '2021-12-31' # Time window end
-def long_years_serie(installation_date, suspension_date):
+def len_years_serie(installation_date, suspension_date):
     if suspension_date:
         diff_date = datetime.strptime(suspension_date, '%Y-%m-%d') - datetime.strptime(installation_date, '%Y-%m-%d')
     else:
@@ -26,7 +26,7 @@ print('\nBasic date difference sample with only date end time window cut\n'
       'Installation date: %s\n' %(installation_date)+
       'Suspension date: %s\n' %(suspension_date)+
       'Time-window end date: %s\n' %(tw_end_date)+
-      'Serie length, years: %f' %long_years_serie(installation_date,suspension_date))
+      'Serie length, years: %f' %len_years_serie(installation_date,suspension_date))
 
 
 # Basic date difference sample with full date end time window cut
@@ -38,7 +38,7 @@ tw_start_date = '' # Time-window start. Use '' for set 1900-01-01
 tw_end_date = '2021-12-31' # Time-window end. Use '' for use the current date and prevent over-time wrong suspension dates
 if not tw_start_date: tw_start_date = '1900-01-01'
 if not tw_end_date: tw_end_date = str(datetime.today().date())
-def long_years_serie(installation_date, suspension_date):
+def len_years_serie(installation_date, suspension_date):
     if installation_date:
         if datetime.strptime(installation_date, '%Y-%m-%d') <= datetime.strptime(tw_start_date, '%Y-%m-%d'):
             tw_installation_date = tw_start_date
@@ -61,12 +61,12 @@ def long_years_serie(installation_date, suspension_date):
     else:
         diff_date = 0
         tw_diff_date = 0
-    return diff_date, tw_diff_date
+    return diff_date, tw_diff_date # First value is complete length. Second value is time window length
 print('\nBasic date difference sample with full date end time window cut\n'
       'Installation date: %s\n' %(installation_date)+
       'Suspension date: %s\n' %(suspension_date)+
       'Time-window start date: %s\n' %(tw_start_date)+
       'Time-window end date: %s\n' %(tw_end_date)+
-      'Serie full length, years: %f\n' %long_years_serie(installation_date,suspension_date)[0]+
-      'Serie time-window length, years: %f' %long_years_serie(installation_date,suspension_date)[1])
+      'Serie full length, years: %f\n' %len_years_serie(installation_date,suspension_date)[0]+
+      'Serie time-window length, years: %f' %len_years_serie(installation_date,suspension_date)[1])
 
