ad31a9c5ff8a6ba269ce2c7c66d9a102f419ff03
r.cfdtools <r.cfdtools@gmail.com>
2022-12-27 08:46:37 -0500

    General update.

--- .src/Readme.md
+++ .src/Readme.md
@@ -2,16 +2,29 @@
 
 Esta carpeta contiene scripts y librerías independientes de uso general
 
+
 ### Extensiones
 
 * .py: script o librería de Python
-* .bas: script de Visual Basic for Applications
 * .sh: shell script for Linux 
+
 
 ### Scripts
 
-* CNEStationCSVJoin.py: este script, descomprime múltiples archivos de series .csv contenidos en archivos .zip obtenidas manualmente del [IDEAM](http://dhime.ideam.gov.co/atencionciudadano/) y los une en un único archivo de registros .csv.
-* ChirpsGetValue.py: a través de este script podrá descargar y descomprimir archivos de precipitación Chirps, segmentar las series IDEAM por mes para cada año y leer y registrar los valores correspondientes de precipitación para cada estación y mes. 
+| Script                                       | Descripción                                                                                                                                                                                               | Lenguaje     | Actividad                                                           |
+|----------------------------------------------|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|--------------|---------------------------------------------------------------------|
+| [downloadASTER.sh](downloadASTER.sh)         | Descarga automática de modelos digitales de elevación ASTER GDEM                                                                                                                                          | Shell script | [DEMAster](../Section02/DEMAster)                                   |
+| [downloadSRTM.sh](downloadSRTM.sh)           | Descarga automática de modelos digitales de elevación SRTM                                                                                                                                                | Shell script | [DEMSrtm](../Section02/DEMSrtm)                                     |
+| [downloadALOS.sh](downloadALOS.sh)           | Descarga automática de modelos digitales de elevación ALOS PALSAR                                                                                                                                         | Shell script | [DEMAlos](../Section02/DEMAlos)                                     |
+| [CNEStationCSVJoin.py](CNEStationCSVJoin.py) | Descomprime múltiples archivos de series .csv contenidos en archivos .zip obtenidas manualmente del [IDEAM](http://dhime.ideam.gov.co/atencionciudadano/) y los une en un único archivo de registros .csv | Python       | [CNEStationDatasetDownload](../Section03/CNEStationDatasetDownload) |
+| [ChirpsGetValue.py](ChirpsGetValue.py)       | Obtención de series de datos discretos climatológicos satelitales y correlación con datos terrestres                                                                                                      | Python       | [RemoteSensing](../Section03/RemoteSensing)                         |
+| [EDA.py](EDA.py)                             | Exploración y análisis de series - EDA - Representación gráfica                                                                                                                                           | Python       | [EDA](../Section03/EDA)                                             |
+| [Outlier.py](Outlier.py)                     | Identificación y procesamiento de datos atípicos - Outliers                                                                                                                                               | Python       | [Outlier](../Section03/Outlier)                                     |
+| [Impute.py](Impute.py)                       | Completado y extendido de series - Imputación                                                                                                                                                             | Python       | [Impute](../Section03/Impute)                                       |
+| [ENSOONI.py](ENSOONI.py)                     | Análisis de cambio climático para segmentación de series                                                                                                                                                  | Python       | [ENSOONI](../Section03/ENSOONI)                                     |
+| [Agg.py](Agg.py)                             | Agregación estadística para obtención de promedios multianuales compuestos y por fenómeno climatológico                                                                                                   | Python       | [Agg](../Section03/Agg)                                             |
+| [Test.py](Test.py)                           | Archivo para pruebas de ejecución                                                                                                                                                                         | Python       | N/A                                                                 |
+
 
 ##
 
@@ -19,5 +32,5 @@ _R.LTWB es de uso libre para fines académicos, conoce nuestra licencia, cláusu
 
 _¡Encontraste útil este repositorio!, apoya su difusión marcando este repositorio con una ⭐ o síguenos dando clic en el botón Follow de [rcfdtools](https://github.com/rcfdtools) en GitHub._
 
-| [:house: Inicio](https://github.com/rcfdtools/R.LTWB) |
-|-------------------------------------------------------|
+| [:house: Inicio](../Readme.md) |
+|--------------------------------|
--- .src/test.py
+++ .src/test.py
@@ -1,4 +1,143 @@
-import sys
-print('Curso LTWB'
-      '\nPython version: ',
-      sys.version)
+print(range(12))
+
+'''
+# -*- coding: UTF-8 -*-
+# Parameters
+thermal_level_caldas = False  # True for Caldas classification, False for conventional classification range
+thermal_level_ref_conv = [[1000,'Cálido, 24°C+, <= 1000 meters'],
+                          [2000,'Templado, 18°C+, <= 2000 meters'],
+                          [3000,'Frío, 12°C+, <= 3000 meters'],
+                          [4000,'Páramo, 0°C, <= 4000 meters'],
+                          [99999,'Glacial, 0°C-, > 4000 meters']] # Elevation value in meters
+thermal_level_ref_caldas = [[800,'Cálido, T>=24°C, <=800meter'],
+                            [1800,'Templado, 24°C>T>18°C, <=1800meter'],
+                            [2800,'Frío, 18°C>T>12°C, <=2800meter'],
+                            [3700,'Muy Frío, 12°C>T>6°C, <=3700meter'],
+                            [4700,'Extremadamente Frio, 6°C>T>0°C, <=4700meter'],
+                            [99999,'Nival, T<0°C, >4700meter']] # Elevation value in meters
+
+# Thermal level system
+if thermal_level_caldas == True:
+    thermal_level_ref = thermal_level_ref_caldas
+else:
+    thermal_level_ref = thermal_level_ref_conv
+
+def thermal_level_f(elevation):
+    for i in thermal_level_ref[:]:
+        if elevation <= i[0]:
+            return i[0],i[1]
+
+elevation_value = 1825
+print('The elevation value %d correspond to the Thermic level value: %s ' %(elevation_value, thermal_level_f(elevation_value)[1]))
+
+
+
+from datetime import datetime
+installation_date = '14/09/1972'
+#installation_date = ''
+#suspension_date = '02/07/2019'
+suspension_date = ''
+tw_end_date = '31/12/2021' # Time window end
+def len_years_serie(installation_date, suspension_date):
+    if not installation_date:
+        installation_date = tw_end_date
+        suspension_date = tw_end_date
+    if not suspension_date:
+        suspension_date = tw_end_date
+    diff_date = datetime.strptime(suspension_date, '%d/%m/%Y') - datetime.strptime(installation_date, '%d/%m/%Y')
+    return float(diff_date.days)/365
+
+print(len_years_serie(installation_date, suspension_date))
+
+# Basic date difference sample over Python 3
+installation_date = datetime.strptime('14/09/1972', '%d/%m/%Y')
+#installation_date = ''
+suspension_date = datetime.strptime('02/07/2019', '%d/%m/%Y')
+#suspension_date = ''
+tw_end_date = datetime.strptime('31/12/2021', '%d/%m/%Y') # Time window end
+is_python3 = True # Insert False for Python 2
+def len_years_serie(installation_date, suspension_date):
+    if not installation_date:
+        installation_date = tw_end_date
+    if not suspension_date:
+        suspension_date = tw_end_date
+    if is_python3:
+        diff_date = suspension_date - installation_date
+    else:
+        diff_date = datetime.strptime(suspension_date, '%d/%m/%Y') - datetime.strptime(installation_date, '%d/%m/%Y')
+    print('diff_date: %s\n' %(diff_date)+
+          'Type: %s' %(type(diff_date)))
+    return float(diff_date.days)/365
+print('len_years_serie: %f' %(len_years_serie(installation_date, suspension_date)))
+
+
+# Basic date difference sample
+#import dateutil.utils
+installation_date = '1972-09-14'
+suspension_date = '2019-07-02'
+diff_date = datetime.strptime(suspension_date, '%Y-%m-%d') - datetime.strptime(installation_date, '%Y-%m-%d')
+print('Basic date difference sample\n'
+      'Installation date: %s\n' %(installation_date)+
+      'Suspension date: %s\n' %(suspension_date)+
+      'Serie length, years: %f' %(diff_date.days/365))
+
+
+# Basic date difference sample with only date end time window cut
+installation_date = '1972-09-14'
+suspension_date = '' # Use '' for not suspension date
+tw_end_date = '2021-12-31' # Time window end
+def len_years_serie(installation_date, suspension_date):
+    if suspension_date:
+        diff_date = datetime.strptime(suspension_date, '%Y-%m-%d') - datetime.strptime(installation_date, '%Y-%m-%d')
+    else:
+        diff_date = datetime.strptime(tw_end_date, '%Y-%m-%d') - datetime.strptime(installation_date, '%Y-%m-%d')
+    return float(diff_date.days)/365
+print('\nBasic date difference sample with only date end time window cut\n'
+      'Installation date: %s\n' %(installation_date)+
+      'Suspension date: %s\n' %(suspension_date)+
+      'Time-window end date: %s\n' %(tw_end_date)+
+      'Serie length, years: %f' %len_years_serie(installation_date,suspension_date))
+
+
+# Basic date difference sample with full date end time window cut
+# The function don't check leap year and divide the dates difference between 365
+# Date values format must be like YYYY-mm-dd
+installation_date = '1972-09-14' # 1972-09-14
+suspension_date = '' # Use '' for not suspension date
+tw_start_date = '' # Time-window start. Use '' for set 1900-01-01
+tw_end_date = '2021-12-31' # Time-window end. Use '' for use the current date and prevent over-time wrong suspension dates
+if not tw_start_date: tw_start_date = '1900-01-01'
+if not tw_end_date: tw_end_date = str(datetime.today().date())
+def len_years_serie(installation_date, suspension_date):
+    if installation_date:
+        if datetime.strptime(installation_date, '%Y-%m-%d') <= datetime.strptime(tw_start_date, '%Y-%m-%d'):
+            tw_installation_date = tw_start_date
+        else:
+            tw_installation_date = installation_date
+        if suspension_date:
+            if datetime.strptime(suspension_date, '%Y-%m-%d') >= datetime.strptime(tw_end_date, '%Y-%m-%d'):
+                tw_suspension_date = tw_end_date
+            else:
+                tw_suspension_date = suspension_date
+            diff_date = datetime.strptime(suspension_date, '%Y-%m-%d') - datetime.strptime(installation_date, '%Y-%m-%d')
+            tw_diff_date = datetime.strptime(tw_suspension_date, '%Y-%m-%d') - datetime.strptime(tw_installation_date, '%Y-%m-%d')
+        else:
+            diff_date = datetime.strptime(tw_end_date, '%Y-%m-%d') - datetime.strptime(installation_date, '%Y-%m-%d')
+            tw_diff_date = datetime.strptime(tw_end_date, '%Y-%m-%d') - datetime.strptime(tw_installation_date, '%Y-%m-%d')
+        diff_date = float(diff_date.days)/365
+        tw_diff_date = float(tw_diff_date.days)/365
+        if diff_date < 0: diff_date = 0
+        if tw_diff_date < 0: tw_diff_date = 0
+    else:
+        diff_date = 0
+        tw_diff_date = 0
+    return diff_date, tw_diff_date # First value is complete length. Second value is time window length
+print('\nBasic date difference sample with full date end time window cut\n'
+      'Installation date: %s\n' %(installation_date)+
+      'Suspension date: %s\n' %(suspension_date)+
+      'Time-window start date: %s\n' %(tw_start_date)+
+      'Time-window end date: %s\n' %(tw_end_date)+
+      'Serie full length, years: %f\n' %len_years_serie(installation_date,suspension_date)[0]+
+      'Serie time-window length, years: %f' %len_years_serie(installation_date,suspension_date)[1])
+'''
+
